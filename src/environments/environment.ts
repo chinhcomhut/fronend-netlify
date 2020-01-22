e75f207e432254ed40f5d0e4d27116106d919ac5
chinhcomhut <chinhcomhut@gmail.com>
2020-01-23 00:39:50 +0700

    hehe

---
+++ src/environments/environment.ts
@@ -0,0 +1,19 @@
+
+export const environment = {
+  production: false,
+  productUrl: 'http://nguyendongminhtri.herokuapp.com/api/auth/product/',
+  productUploadFileUrl: 'http://nguyendongminhtri.herokuapp.com/api/auth/file/',
+  lineUrl: 'http://nguyendongminhtri.herokuapp.com/api/auth/line/',
+  userUrl: 'http://nguyendongminhtri.herokuapp.com/api/auth/user/',
+  commentUrl: 'http://nguyendongminhtri.herokuapp.com/api/auth/comment/',
+  albumUrl: 'http://nguyendongminhtri.herokuapp.com/api/auth/album/',
+  albumUploadAvatarUrl: 'http://nguyendongminhtri.herokuapp.com/api/auth/album-avatar/',
+  albumUploadImageUrl: 'http://nguyendongminhtri.herokuapp.com/api/auth/album-add-image/',
+  imageUrl: 'http://nguyendongminhtri.herokuapp.com/api/auth/image/',
+  UserAvatarUrl: 'http://localhost:8080/api/auth/user-avatar/',
+
+  loginUrl: 'http://nguyendongminhtri.herokuapp.com/api/auth/signin',
+  signupUrl: 'http://nguyendongminhtri.herokuapp.com/api/auth/signup',
+  updateProfileUrl: 'http://nguyendongminhtri.herokuapp.com/api/auth/update-profile',
+  updatePasswordUrl: 'http://nguyendongminhtri.herokuapp.com/api/auth/update-password',
+};
